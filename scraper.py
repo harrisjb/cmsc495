a560a7d65f094799f87eff0d32c6b584b8472662
dspringer612 <david.k.springer@colorado.edu>
2018-07-01 23:48:46 -0400

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,24 +1,29 @@
 """
 Filename: scraper.py
 Author: Dave Springer
-Date:
+Date: July 1, 2018
+Current Version: 0.1.6
 Description: This file contains functions to identify available sessions and to
 scrape course data for a specified session.  Creates a JSON file holding the
 course information in a format that can be understood by the database importer.
 
 Revision History:
-
-0.1.5   06/30/2018  Modified scrape so it takes a session name as an argument.
-                    Scrape method now produces a JSON file for course data.
-0.1.4   06/29/2018  Updated getSessions function so that a global dictionary
-                    that maps the session name to the session ID is added
-0.1.3   06/28/2018  Added function to determine which sessions are available
-0.1.2   06/27/2018  Updated scrape function so that course attributes are scaped
-                    and added to the course's dictionary
-0.1.1   06/26/2018  Added scrape function which opens connection to UMUC's
-                    schedule web page for a pre-specified URL for Fall 2018 and
-                    creates a list of courses with empty dictionaries for each
-                    course.
+#       Date        By          Description
+0.1.6	07/01/2018	Springer	Minor modifications to formatting/comments
+0.1.5   06/30/2018  Springer    Modified scrape so it takes a session name as an
+                                argument. Scrape method now produces a JSON file
+                                for course data.
+0.1.4   06/29/2018  Springer    Updated getSessions function so that a global
+                                dictionary that maps the session name to the
+                                session ID is added
+0.1.3   06/28/2018  Springer    Added function to determine which sessions are
+                                available Updated scrape function so that course
+                                attributes are scraped and added to the course's
+                                dictionary
+0.1.1   06/26/2018  Springer    Added scrape function which opens connection to
+                                UMUC's schedule web page for a pre-specified URL
+                                for Fall 2018 and creates a list of courses with
+                                empty dictionaries for each course.
 """
 
 from urllib.request import urlopen
