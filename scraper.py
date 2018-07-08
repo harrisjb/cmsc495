77d9003f053f3998391fe7e3159f382b8eee0a15
dspringer612 <david.k.springer@colorado.edu>
2018-07-08 17:48:34 -0400

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -9,18 +9,27 @@ course information in a format that can be understood by the database importer.
 
 Revision History:
 #       Date        By          Description
-0.1.6	07/01/2018	Springer	Minor modifications to formatting/comments
-0.1.5   06/30/2018  Springer    Modified scrape so it takes a session name as an
+0.2.3   07/08/2018  Springer    Modified getSessions() function to look for
+                                sub-semesters rather than just entire semesters
+0.2.2   07/06/2018  Springer    Extracted prerequisites from description and
+                                updated description so that it does not include
+                                this information
+0.2.1   07/05/2018  Springer    Extracted faculty first and last name from full
+                                faculty field
+0.2.0   07/03/2018  Springer    Extracted start and end date from string of
+                                dates
+0.1.4	07/01/2018	Springer	Minor modifications to formatting/comments
+0.1.3   06/30/2018  Springer    Modified scrape so it takes a session name as an
                                 argument. Scrape method now produces a JSON file
                                 for course data.
-0.1.4   06/29/2018  Springer    Updated getSessions function so that a global
+0.1.2   06/29/2018  Springer    Updated getSessions function so that a global
                                 dictionary that maps the session name to the
                                 session ID is added
-0.1.3   06/28/2018  Springer    Added function to determine which sessions are
+0.1.1   06/28/2018  Springer    Added function to determine which sessions are
                                 available Updated scrape function so that course
                                 attributes are scraped and added to the course's
                                 dictionary
-0.1.1   06/26/2018  Springer    Added scrape function which opens connection to
+0.1.0   06/26/2018  Springer    Added scrape function which opens connection to
                                 UMUC's schedule web page for a pre-specified URL
                                 for Fall 2018 and creates a list of courses with
                                 empty dictionaries for each course.
@@ -31,7 +40,7 @@ from bs4 import BeautifulSoup
 import re
 import json
 
-__sessions = dict()
+__sessions = {}
 __baseURL = 'http://webapps.umuc.edu/soc/us.cfm'
 
 def getSessions():
@@ -49,17 +58,34 @@ def getSessions():
     bs = BeautifulSoup(html.read(), 'html.parser')
 
     # Find each term and add it to the dictionary
+    semmesterID = ''
+    semmesterName = ''
     for session in bs.find('select', {'id': 'soc-session'}).find_all('option'):
 
-        sessionID = session.get('value')
+        # Splitting by '|' gives us two values-- semesterID and subSessionID
+        sessionInfo = session.get('value').split('|')
+        # If no subSession ID, then this is for the entire semester
+        if len(sessionInfo) == 1:
+            semmesterID = sessionInfo[0]
+            sessionID = semmesterID
 
-        # Only add full terms (not sub terms)
-        if len(sessionID) == 4:
-            pattern = '([0-9]* [a-zA-Z]*)'
-            sessionName = re.match(pattern, session.get_text()).groups()[0]
-            __sessions[sessionName] = sessionID
+            # Get session name
+            pattern = '[0-9]* [A-Za-z]*'
+            semmesterName = re.search(pattern, session.get_text()).group()
+            sessionName = semmesterName
+        else:
+            sessionID = semmesterID + '%7C' + sessionInfo[1]
 
-    return list(__sessions.keys())
+            # Get session name
+            pattern = '[A-Za-z]+( [A-Za-z0-9]+)*'
+            sessionName = semmesterName + ' ' + \
+                          re.search(pattern, session.get_text()).group()
+
+        # Add course to dictionary
+        __sessions[sessionName] = sessionID
+
+
+    #return list(__sessions.keys())
 
 def scrape(sessionName):
 
@@ -92,19 +118,47 @@ def scrape(sessionName):
         row2 = course.next_sibling.next_sibling
         desc = row2.get_text()[1:]
 
+        # Extract prerequisites from description
+        match = re.match('(.*?)Prereq.*?: (.*?)\. (.*)', desc)
+        if match:
+            prereq = match.group(2)
+            description = match.group(1) + match.group(3)
+        else:
+            prereq = ''
+            description = desc
+
         # Row 3: Class No., Section, Start/End, Day/Time, Status, Location
         row3 = row2.next_sibling.next_sibling
         classNum = row3.find('td', {'headers':'soc-hd-class'}).get_text()
         section = row3.find('td', {'headers':'soc-hd-goarmyed'}).get_text()
-        date = row3.find('td', {'headers': 'soc-hd-date'}).get_text()
+        dates = row3.find('td', {'headers': 'soc-hd-date'})
         day = row3.find('td', {'headers': 'soc-hd-day'}).get_text()
         time = row3.find('td', {'headers': 'soc-hd-time'}).get_text()
         status = row3.find('td', {'headers': 'soc-hd-status'}).get_text()
-        location = row3.find('td', {'headers': 'soc-hd-location'}).get_text()
+        loc = row3.find('td', {'headers': 'soc-hd-location'}).get_text()
+
+        # Format location for in-person classes (keep online as is)
+        if loc == 'Online':
+            location = loc
+        else:
+            location = loc.split('-')[0]
+
+        # Get start/end dates from dates string
+        dates = dates.get_text().split('-')
+        start = dates[0]
+        end = dates[1]
 
         # Row 4: Faculty information
         row4 = row3.next_sibling.next_sibling
         name = row4.find('td', {'colspan':'3'}).get_text()
+        name = name[9:] # Strip "Faculty" header
+        names = name.split(', ')
+        if len(names) > 1: # Only if there is a faculty member listed
+            last = names[0]
+            first = names[1].split(' ')[0]
+        else: # Otherwise, use default values
+            last = ''
+            first = ''
 
         # Make dictionary for course information
         info = {
@@ -114,17 +168,16 @@ def scrape(sessionName):
             'credits' : int(numCredits),
             'classNum' : int(classNum),
             'section' : int(section),
-            'startDate' : date,
-            'endDate' : date,
+            'startDate' : start,
+            'endDate' : end,
             'day' : day,
             'time' : time,
             'status' : status,
             'location' : location,
-            'facultyFirst' : name,
-            'facultyLast' : name,
-            'facultyMI' : name,
-            'description' : desc,
-            'prereq' : ''
+            'facultyFirst' : first,
+            'facultyLast' : last,
+            'description' : description,
+            'prereq' : prereq
         }
 
         courses.append(info)
@@ -136,7 +189,7 @@ def scrape(sessionName):
     json.dump(data, fp=open('courses.json', 'w'), indent=4)
 
 
-def testOutput(sessionName):
+def __testOutput(sessionName):
     """
     This function is used to test that information has been scraped as expected
     """
@@ -156,8 +209,8 @@ def testOutput(sessionName):
         data = json.load(file)
 
     # Print JSON data (Note: may not print all due to size. Verify in file.)
-    print(json.dumps(data, indent=4))
+    #print(json.dumps(data, indent=4))
 
 
 # Test that all functions are working as expected
-testOutput('2018 Fall')
+__testOutput('2018 Fall US Session 2')
